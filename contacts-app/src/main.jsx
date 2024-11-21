ca5895658078aa8a25b4f2d110798d5665d7aab2
zekariasfe <zekariasfekeru@gmail.com>
2024-11-21 19:54:42 +0300

    hhh

---
+++ contacts-app/src/main.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  
+  Children: [
+    {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+  ],
+},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
